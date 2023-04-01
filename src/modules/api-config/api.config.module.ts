9528d6f9683d62a7b34aa0fed365b2054d236ea8
VwoLFy <v_wolfy@mail.ru>
2023-04-01 15:07:42 +0300

    build(api): remove hapi/joi & add joi
    fix(api): add property "esModuleInterop": true in tsconfig
    
    feat(apiConfigModule): add env validation

--- src/modules/api-config/api.config.module.ts
+++ src/modules/api-config/api.config.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ApiConfigService } from './api.config.service';
 import { ConfigModule } from '@nestjs/config';
 import { configuration } from './configuration';
+import Joi from 'joi';
 
 @Module({
   imports: [
@@ -9,28 +10,44 @@ import { configuration } from './configuration';
       isGlobal: true,
       load: [configuration],
 
-      // validationSchema: Joi.object({
-      //   //NODE_ENV: Joi.string().valid('development', 'production', 'test', 'provision').default('development'),
-      //   PORT: Joi.number(),
-      //
-      //   TYPEORM_USERNAME: Joi.string().required(),
-      //   TYPEORM_PASSWORD: Joi.string().required(),
-      //
-      //   JWT_SECRET_FOR_ACCESSTOKEN: Joi.string().required(),
-      //   EXPIRES_IN_TIME_OF_ACCESSTOKEN: Joi.string().required(),
-      //   JWT_SECRET_FOR_REFRESHTOKEN: Joi.string().required(),
-      //   EXPIRES_IN_TIME_OF_REFRESHTOKEN: Joi.string().required(),
-      //
-      //   SA_LOGIN: Joi.string().required(),
-      //   SA_PASSWORD: Joi.string().required(),
-      //
-      //   EMAIL_PASSWORD: Joi.string().required(),
-      //   EMAIL: Joi.string().email().required(),
-      //   EMAIL_FROM: Joi.string().required(),
-      //   MY_EMAIL: Joi.string().email().required(),
-      //
-      //   IP_RESTRICTION: Joi.boolean(),
-      // }),
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test', 'provision')
+          .default('development'),
+        PORT: Joi.number(),
+
+        CURRENT_APP_BASE_URL: Joi.string(),
+
+        //PGSQL_URL: Joi.string(),
+
+        //MAIL_USER: Joi.string().required(),
+        //MAIL_PASSWORD: Joi.string().required(),
+        //MAIL_FROM: Joi.string().required(),
+
+        CLIENT_URL: Joi.string().required(),
+
+        //ACCESS_TOKEN_SECRET: Joi.string().required(),
+        //EXPIRED_ACCESS: Joi.string().required(),
+        //REFRESH_TOKEN_SECRET: Joi.string().required(),
+        //EXPIRED_REFRESH: Joi.string().required(),
+
+        //SECRET_ACCESS_KEY: Joi.string(),
+        //ACCESS_KEY_ID: Joi.string(),
+        //BUCKET: Joi.string(),
+        //ENDPOINT: Joi.string(),
+
+        //TOKEN_NGROK: Joi.string(),
+
+        //TOKEN_TELEGRAM: Joi.string(),
+
+        //API_KEY_STRIPE: Joi.string(),
+        //SECRET_HOOK_STRIPE: Joi.string(),
+
+        SA_LOGIN: Joi.string().required(),
+        SA_PASSWORD: Joi.string().required(),
+
+        IP_RESTRICTION: Joi.boolean(),
+      }),
     }),
   ],
   providers: [ApiConfigService],
