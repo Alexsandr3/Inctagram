15d616624b8cfee0897bd153b895090f1ce6c6cc
Alexsandr3 <fam_86@mail.ru>
2023-04-02 13:56:57 +0200

    feat(typeOrm): add config for migration

--- src/providers/database/database.module.ts
+++ src/providers/database/database.module.ts
@@ -1,15 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DefaultNamingStrategy, NamingStrategyInterface } from 'typeorm';
 import { ApiConfigModule } from '../../modules/api-config/api.config.module';
 import { ApiConfigService } from '../../modules/api-config/api.config.service';
 import DatabaseLogger from './utils/databaseLogger';
-
-export class PluralNamingStrategy extends DefaultNamingStrategy implements NamingStrategyInterface {
-  tableName(className: string, customName?: string): string {
-    return customName || className.toLowerCase() + 's';
-  }
-}
 
 @Module({
   imports: [
