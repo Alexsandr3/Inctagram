90812fbbb84ec878965ae5154247e8b2d222efe2
VwoLFy <v_wolfy@mail.ru>
2023-04-14 12:14:44 +0300

    refactor(user): prisma models, code in use cases, entities, repo ]

--- src/modules/users/api/users.controller.ts
+++ src/modules/users/api/users.controller.ts
@@ -26,23 +26,20 @@ import { ApiBearerAuth } from '@nestjs/swagger';
 import { CommandBus } from '@nestjs/cqrs';
 import { UploadImageAvatarCommand } from '../aplication/use-cases/upload-image-avatar.use-case';
 import { NotificationException, ResultNotification } from '../../../main/validators/result-notification';
-import { CreateProfileCommand } from '../aplication/use-cases/create-profile.use-case';
-import { ResultNotification } from '../../../main/validators/result-notification';
-
 import { CreateProfileInputDto } from './inpu-dto/create-profile.input.dto';
 import { ProfileViewDto } from './view-models/profile-view.dto';
 import { JwtAuthGuard } from '../../auth/api/guards/jwt-auth.guard';
 import { UpdateProfileCommand } from '../aplication/use-cases/update-profile.use-case';
-import { IProfilesRepository } from '../infrastructure/profiles.repository';
 import { NotificationCode } from '../../../configuration/exception.filter';
 import { CheckerNotificationErrors } from '../../../main/validators/checker-notification.errors';
 import { ValidationTypeImagePipe } from '../../../main/validators/validation-type-image.pipe';
+import { IUsersRepository } from '../infrastructure/users.repository';
 
 @UseGuards(JwtAuthGuard)
 @ApiBearerAuth()
 @Controller('users')
 export class UsersController {
-  constructor(private readonly commandBus: CommandBus) {}
+  constructor(private readonly commandBus: CommandBus, private readonly usersRepository: IUsersRepository) {}
 
   @SwaggerDecoratorsByCreateProfile()
   @Post('/profile')
@@ -68,13 +65,14 @@ export class UsersController {
   @Get(`/profile/:id`)
   @HttpCode(HTTP_Status.OK_200)
   async getProfile(@Param('id', ParseIntPipe) userId: number): Promise<ProfileViewDto> {
-    const profile = await this.profileRepo.findById(userId);
+    const user = await this.usersRepository.findById(userId);
     const notification = new ResultNotification<ProfileViewDto>();
     notification.addErrorFromNotificationException(
       new NotificationException(`Profile not found with ${userId}`, 'profile', NotificationCode.NOT_FOUND),
     );
-    if (!profile) throw new CheckerNotificationErrors('Error', notification);
-    return ProfileViewDto.createView(profile);
+    console.log('user in get---------', user);
+    if (!user) throw new CheckerNotificationErrors('Error', notification);
+    return ProfileViewDto.createView(user.profile, user.userName);
   }
 
   /**
