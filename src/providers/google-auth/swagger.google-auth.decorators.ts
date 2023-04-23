2d4f7492ce2266874c84a7c94396368a263ea8e2
Alexsandr3 <fam_86@mail.ru>
2023-04-23 16:54:53 +0200

    feat(OAuth-2): added swagger decorators for Google auth

---
+++ src/providers/google-auth/swagger.google-auth.decorators.ts
@@ -0,0 +1,30 @@
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { applyDecorators } from '@nestjs/common';
+import { HTTP_Status } from '../../main/enums/http-status.enum';
+import { TokenTypeSwaggerDto } from '../../configuration/swagger/helpers/token-type-swagger.dto';
+
+export function SwaggerDecoratorsByLoginWithGoogle(): MethodDecorator {
+  return applyDecorators(
+    ApiOperation({ summary: 'Try login user to the system with google' }),
+    ApiResponse({
+      status: HTTP_Status.OK_200,
+      description: 'success, return access token (body) and refresh token(cookie)',
+      type: TokenTypeSwaggerDto,
+    }),
+    ApiResponse({ status: HTTP_Status.UNAUTHORIZED_401, description: 'Unauthorized' }),
+    ApiResponse({
+      status: HTTP_Status.TOO_MANY_REQUESTS_429,
+      description: 'More than 5 attempts from one IP-address during 10 seconds',
+    }),
+  );
+}
+
+export function SwaggerDecoratorsByAuthGoogle(): MethodDecorator {
+  return applyDecorators(
+    ApiOperation({ summary: 'Redirect to google auth' }),
+    ApiResponse({
+      status: HTTP_Status.OK_200,
+      description: 'success',
+    }),
+  );
+}
