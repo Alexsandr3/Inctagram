55fdb7cb45a61026547cae725a7a2ccc70c7d6e0
Alexsandr3 <fam_86@mail.ru>
2023-05-08 17:09:44 +0200

    test(subscriptions): added  subscriptions helper with some tests

---
+++ test/helpers/subscriptions-helper.ts
@@ -0,0 +1,28 @@
+import { INestApplication } from '@nestjs/common';
+import { HTTP_Status } from '../../src/main/enums/http-status.enum';
+import request from 'supertest';
+import { CreateSubscriptionInputDto } from '../../src/modules/subscriptions/api/input-dtos/create-subscription-input.dto';
+import { subscriptionsEndpoints } from '../../src/modules/subscriptions/api/routing/subscriptions.routing';
+
+export class SubscriptionsHelper {
+  constructor(private readonly app: INestApplication) {}
+
+  async createSubscription(
+    command: CreateSubscriptionInputDto,
+    config: {
+      token?: any;
+      expectedCode?: number;
+    } = {},
+  ) {
+    // default expected code is 201 or code mistake from config
+    const expectedCode = config.expectedCode ?? HTTP_Status.CREATED_201;
+    // send request for create user
+    const response = await request(this.app.getHttpServer())
+      .post(subscriptionsEndpoints.createSubscriptions())
+      .auth(config.token, { type: 'bearer' })
+      .send(command)
+      .expect(expectedCode);
+
+    return response.body;
+  }
+}
