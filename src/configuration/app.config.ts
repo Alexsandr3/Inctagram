aca4269fbd2a02f60d218b98837201554674e920
Alexsandr3 <fam_86@mail.ru>
2023-05-08 22:37:42 +0200

    build(package): change ver to 16

--- src/configuration/app.config.ts
+++ src/configuration/app.config.ts
@@ -3,7 +3,6 @@ import { pipeSetup } from './pipe.setup';
 import { exceptionFilterSetup } from './exception-filter.setup';
 import cookieParser from 'cookie-parser';
 import CustomLogger from '../providers/logger/customLogger';
-import { ApiConfigService } from '../modules/api-config/api.config.service';
 
 /**
  * Need for use without testing
@@ -15,9 +14,17 @@ export const appConfig = (app: INestApplication) => {
   //use custom logger
   app.useLogger(app.get(CustomLogger));
   //add cors
-  const corsOptions = app.get(ApiConfigService).CORS_ORIGIN;
+  // const corsOptions = app.get(ApiConfigService).CORS_ORIGIN;
   app.enableCors({
-    origin: corsOptions,
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'http://localhost:5003',
+      'http://localhost:5000',
+      'http://localhost:63342',
+      'https://inctagram-develop.vercel.app',
+      'https://inctagram-main.vercel.app',
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   });
