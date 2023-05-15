8a6654c903b1323658960b734ac4c77283fc3889
Alexsandr3 <fam_86@mail.ru>
2023-05-15 13:45:03 +0200

    feat(auth): added new field for me

--- src/modules/auth/api/view-dto/me.view.dto.ts
+++ src/modules/auth/api/view-dto/me.view.dto.ts
@@ -7,10 +7,12 @@ export class MeViewDto {
   userId: number;
   userName: string;
   email: string;
+  hasBusinessAccount: boolean;
 
   constructor(user: User) {
     this.userId = user.id;
     this.userName = user.userName;
     this.email = user.email;
+    this.hasBusinessAccount = user.hasBusinessAccount;
   }
 }
