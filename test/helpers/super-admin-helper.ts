bf2d6322563510a645a676c9046608a25fa31f2f
Alexsandr3 <fam_86@mail.ru>
2023-05-22 13:46:48 +0200

    test(super-admin): added authorization

--- test/helpers/super-admin-helper.ts
+++ test/helpers/super-admin-helper.ts
@@ -2,29 +2,23 @@ import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { DocumentNode } from 'graphql/language';
 
-// import request from 'supertest-graphql';
-
 export class SuperAdminHelper {
   constructor(private readonly app: INestApplication) {}
 
-  async getUsers<T>(
-    command: DocumentNode,
-    config: {
-      expectedBody?: any;
-    } = {},
-  ): Promise<any> {
+  async getUsers<T>(command: DocumentNode): Promise<any> {
     // send request for create user
-    const response = await request(this.app.getHttpServer()).post('/graphql').send({ query: command.loc?.source.body });
+    const response = await request(this.app.getHttpServer())
+      .post('/graphql')
+      .auth(`admin@admin.me`, `admin`, { type: 'basic' })
+      .send({ query: command.loc?.source.body });
     return response.body.data;
   }
 
-  async deleteUser(
-    command: string,
-    config: {
-      expectedBody?: any;
-    } = {},
-  ): Promise<any> {
-    const response = await request(this.app.getHttpServer()).post('/graphql').send({ query: command });
+  async deleteUser(command: string): Promise<any> {
+    const response = await request(this.app.getHttpServer())
+      .post('/graphql')
+      .auth(`admin@admin.me`, `admin`, { type: 'basic' })
+      .send({ query: command });
     return response.body.data;
   }
 }
