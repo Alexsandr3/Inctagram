b0499632309b4540085dafa928f324e64b88ed6e
Alexsandr3 <fam_86@mail.ru>
2023-05-29 17:47:41 +0200

    feat(subscription): added paypal logic for the beginer ))

--- src/providers/payment/api/stripe.controller.ts
+++ src/providers/payment/api/stripe.controller.ts
@@ -1,16 +1,12 @@
-import { Controller, Get, HttpCode, Post } from '@nestjs/common';
+import { Controller, HttpCode, Post } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { PaymentInputData, Signature } from '../../../main/decorators/signature-data.decorator';
 import { StripePaymentWebhookService } from '../application/stripe-payment-webhook.service';
-import { PaymentPaypalService } from '../application/payment-paypal.service';
 
 @ApiTags('Payments')
 @Controller('payments')
 export class StripeController {
-  constructor(
-    private readonly stripePaymentWebhookService: StripePaymentWebhookService,
-    private readonly paymentPaypalService: PaymentPaypalService,
-  ) {}
+  constructor(private readonly stripePaymentWebhookService: StripePaymentWebhookService) {}
 
   /**
    * Webhook for Stripe Api (see stripe official documentation)
@@ -22,9 +18,5 @@ export class StripeController {
   @Post('stripe/webhook')
   async stripeHook(@Signature() inputData: PaymentInputData) {
     await this.stripePaymentWebhookService.createEventSession(inputData.signature, inputData.body);
-  }
-  @Get()
-  async test() {
-    return this.paymentPaypalService.testCreateSession2();
   }
 }
