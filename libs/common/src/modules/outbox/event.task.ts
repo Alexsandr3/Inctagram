764bebbe5df5cd719dd6242f63500e85cd26d23e
AlexBigBee <alexfamas67@gmail.com>
2023-06-29 12:05:34 +0200

    feat(super-admin): added method get images for user

--- libs/common/src/modules/outbox/event.task.ts
+++ libs/common/src/modules/outbox/event.task.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { OutboxProcessor } from '@common/modules/outbox/outbox.processor';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
 export class EventTask {
   constructor(private readonly outboxProcessor: OutboxProcessor) {}
 
-  // @Cron(CronExpression.EVERY_5_SECONDS)
-  //todo: change to 5 minutes
+  @Cron(CronExpression.EVERY_5_SECONDS)
   public async handlerOutboxEvent(): Promise<void> {
     await this.outboxProcessor.handleOutboxEvent();
   }
